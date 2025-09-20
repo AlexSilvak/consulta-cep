6118e97c70b5a3d3a8dbf4a40d40be87236ef314
AlexSilvak <alex.silvak23@gmail.com>
2025-09-20 19:16:32 -0300

    Criado tabela enderecos_viacep

---
+++ tabela_endereco_viacep.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE enderecos_viacep (
+  cep           VARCHAR2(10),
+  state         VARCHAR2(2),
+  city          VARCHAR2(100),
+  neighborhood  VARCHAR2(100),
+  street        VARCHAR2(150),
+  service       VARCHAR2(20)
+);
+
+
+INSERT INTO enderecos_viacep (
+  cep, state, city, neighborhood, street, service
+) VALUES (
+  '89010025', 'SC', 'Blumenau', 'Centro', 'Rua Doutor Luiz de Freitas Melro', 'viacep'
+);
